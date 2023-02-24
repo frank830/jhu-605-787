1b2ab4c5d99274c976c01f5728086c290df124a3
frank830 <35854800+frank830@users.noreply.github.com>
2023-02-23 23:28:15 -0500

    all required requirements for module 6 is done.

--- module6-solution/assignment1-starter-code/app.js
+++ module6-solution/assignment1-starter-code/app.js
@@ -6,11 +6,23 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  $scope.itemNum = 0;
+  $scope.itemNum = ""
   $scope.stateOfBeing = ""
 
   $scope.checkIfTooMuch = function () {
-    $scope.stateOfBeing = $scope.itemNum <= 3 ? "Enjoy!" : "Too much!";
+    if($scope.itemNum.length == 0){
+      $scope.stateOfBeing = "Please enter data first"
+    }else{
+      var splitedList = $scope.itemNum.split(",")
+      var totalItems = 0
+      for (let item of splitedList){
+        console.log(item.trim());
+        if(item.trim() !== ''){
+          totalItems += 1
+        }
+      }
+      $scope.stateOfBeing = totalItems <= 3 ? "Enjoy!" : "Too much!";
+    }
   };
 }
 

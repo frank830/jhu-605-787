6c48a0b47c2f5c69df5d472facc4e786fa34a837
frank830 <35854800+frank830@users.noreply.github.com>
2023-02-24 00:32:04 -0500

    module 6 completed all requirements

--- module6-solution/assignment1-starter-code/app.js
+++ module6-solution/assignment1-starter-code/app.js
@@ -6,22 +6,27 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-  $scope.itemNum = ""
-  $scope.stateOfBeing = ""
+  $scope.itemNum = "";
+  $scope.myColor = "";
+  $scope.myBorder = "";
+  $scope.totalItems = 0;
 
   $scope.checkIfTooMuch = function () {
     if($scope.itemNum.length == 0){
-      $scope.stateOfBeing = "Please enter data first"
+      $scope.stateOfBeing = "Please enter data first";
+      $scope.myColor = "red";
+      $scope.myBorder = "borderRed";
     }else{
-      var splitedList = $scope.itemNum.split(",")
-      var totalItems = 0
+      var splitedList = $scope.itemNum.split(",");
+      var totalItems = 0;
       for (let item of splitedList){
-        console.log(item.trim());
         if(item.trim() !== ''){
-          totalItems += 1
+          totalItems += 1;
         }
       }
       $scope.stateOfBeing = totalItems <= 3 ? "Enjoy!" : "Too much!";
+      $scope.myColor = "green";
+      $scope.myBorder = "borderGreen";
     }
   };
 }

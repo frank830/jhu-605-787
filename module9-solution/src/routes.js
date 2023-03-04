0e28f69fd257b9f3c7658c2fceef27d4c86cb6dd
frank830 <35854800+frank830@users.noreply.github.com>
2023-03-04 18:10:37 -0500

    categories tab working

--- module9-solution/src/routes.js
+++ module9-solution/src/routes.js
@@ -1,34 +1,32 @@
 (function () {
+  'use strict';
 
-  angular.module('MenuApp',['ui.router']);
+  angular.module('MenuApp');
   
   angular.module('MenuApp')
   .config(RoutesConfig);
-  
   RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
   function RoutesConfig($stateProvider, $urlRouterProvider) {
-  
+    
     // Redirect to tab 1 if no other URL matches
     $urlRouterProvider.otherwise('/');
-  
-    // Set up UI states
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'src/templates/home.template.html'
-      })
-      .state('categories', {
-        url: '/categories',
-        templateUrl: 'src/templates/categories.template.html',
-        controller: 'CategoriesController as categoriesList',
-        resolve: {
-          categories: ['MenuDataService', function (MenuDataService) {
-            return MenuDataService.getAllCategories();
-          }]
-        }
-      });
+      // Set up UI states
+      $stateProvider
+          .state('home', {
+              url: '/',
+              templateUrl: 'src/templates/home.template.html'
+          })
+          .state('categories', {
+              url: '/categories',
+              templateUrl: 'src/templates/categories.template.html',
+              controller: 'CategoriesController as categoriesList',
+              resolve: {
+                categories: ['MenuDataService', function (MenuDataService) {
+                  return MenuDataService.getAllCategories();
+                }]
+              }
+          });
   }
-  
   
   })();
   
